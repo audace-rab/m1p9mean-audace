f8c84cefc07aa3ea778e50368de12b4575fc2b89
Audace Rabarison <audace.rabarison@hangarww.com>
2022-03-25 16:44:56 +0300

    init monggose and mongodb

--- server/app.js
+++ server/app.js
@@ -3,15 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./config/mongoose');
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,9 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/user', indexRouter);
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../dist/client")));
@@ -31,7 +31,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const Users = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  Users.find({}, (err,data) => {
+    if(err) console.log(err);
+
+    res.json(data);
+  })
 });
 
 module.exports = router;
